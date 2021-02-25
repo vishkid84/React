37e1138e89fbe81ee2fc21517ada92131bf88b4a
vishkid84 <vishalittr@gmail.com>
2021-02-25 21:43:45 +0000

    Passing method references between components

--- react-first-app/src/App.js
+++ react-first-app/src/App.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import './App.css';
 import Person from './Person/Person';
 
+// ----Method 1------
 
 // function App() {
 //   return (
@@ -11,17 +12,56 @@ import Person from './Person/Person';
 //   );
 // }
 
+// ----Method 2------
+// const App = () =>{
+//     return (
+//             <div className="App">
+//             <h1>I am a new react app</h1>
+//             <Person name="Vis" age="36"></Person>
+//             <Person name="Dhanz" age="35">My hobbies: Racing</Person>
+//             <Person name="Asher" age="3"></Person>
+//             </div>
+//         );
+// }
+
+// ----Method 3------
 class App extends Component{
+    state = {
+        persons: [
+            {name: 'Vis', age: 36},
+            {name: 'Dhanz', age: 35},
+            {name: 'Asher', age: 3}
+        ]
+    }
+
+    switchNameHandler = (newName) => {
+        // alert('Working')
+        // DO NOT DO THIS: this.state.persons[0].name = 'Vishal';
+        this.setState({
+            persons: [
+                {name: newName, age: 36},
+                {name: 'Dhanz', age: 35},
+                {name: 'Asher', age: 3.5}
+            ]
+        })
+    }
+
     render(){
         return (
             <div className="App">
             <h1>I am a new react app</h1>
-            <Person name="Vis" age="36"></Person>
-            <Person name="Dhanz" age="35">My hobbies: Racing</Person>
-            <Person name="Asher" age="3"></Person>
+            <button onClick={() => this.switchNameHandler('Vishal')}>Switch name</button>
+            <Person name={this.state.persons[0].name} age={this.state.persons[0].age} click={this.switchNameHandler.bind(this, 'Vishal Raju')}></Person>
+            <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My hobbies: Racing</Person>
+            <Person name={this.state.persons[2].name} age={this.state.persons[2].age}></Person>
             </div>
         );
     }
 }
 
 export default App;
+
+// Without state, hard coded would be like below
+/*      <Person name="Vis" age="36"></Person>
+        <Person name="Dhanz" age="35">My hobbies: Racing</Person>
+        <Person name="Asher" age="3"></Person> */
--- react-first-app/src/Person/Person.js
+++ react-first-app/src/Person/Person.js
@@ -1,8 +1,12 @@
 import React from 'react';
 
 const person = (props) => {
-    return <p>My name is {props.name} and I am {props.age} years old</p>
-    
+    return (
+        <div>
+            <h4 onClick={props.click}>My name is {props.name} and I am {props.age} years old.</h4>
+            <p>{props.children}</p>
+        </div>
+    )
 }
 
 export default person;
